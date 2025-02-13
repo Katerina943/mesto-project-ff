26512e98de0d34793814edd7ca5258c82519c0a7
Tyurina Ekaterina <kamit04@yandex.ru>
2025-02-13 23:25:57 +0300

    Отпрвка на первое ревью

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Katerina943/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,60 @@
+// загружаем шаблон для создания элемента списка
+const cardTemplate = document.querySelector("#card-template").content;
+const placesItem = cardTemplate.querySelector(".places__item");
+// загружаем основное содержимое страницы
+const mainContent = document.querySelector(".content");
+// загружаем кнопку добавления изображения
+const profileAddButton = mainContent.querySelector(".profile__add-button");
+// загружаем список изображений
+const placesList = mainContent.querySelector(".places__list");
+
+for (let card of initialCards) {
+  placesList.append(createPlacesItemClone(card, deletePlacesItemClone));
+}
+
+// функция созданием элемента списка
+function createPlacesItemClone(initialCard, deletePlacesItem) {
+  // создаём новый элемент списка изображений
+  const placesItemClone = placesItem.cloneNode(true);
+
+  // загружаем изображение из нового элемента
+  const cardImage = placesItemClone.querySelector(".card__image");
+  // проинициализируем изображение
+  cardImage.setAttribute("alt", initialCard.name);
+  cardImage.setAttribute("src", initialCard.link);
+
+  // загружаем кнопку удаления изображения из нового элемента
+  const cardDeleteButton = placesItemClone.querySelector(".card__delete-button");
+  // добавляем обработчик события кнопке удаления изображения
+  cardDeleteButton.addEventListener("click", deletePlacesItem);
+
+  return placesItemClone;
+}
+
+// функция удаления элемента списка
+function deletePlacesItemClone(event) {
+  // проверка вызова события на кнопке удаления
+  if (!event.target.classList.contains("card__delete-button")) return;
+  const cardDeleteButton = event.target;
+  // определяем конкретный элемент списка, который необходимо удалить
+  const placesItemClone = cardDeleteButton.closest(".places__item");
+  // удаляем элемент из списка
+  placesItemClone.remove();
+}
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
+// @todo: Функция создания карточки 
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+
+
+
+
+
